ec404a3624f99db5f3e3704ca4e7dcadc0c0588e
GregoryUrban <turknben@gmail.com>
2019-08-06 21:03:33 -0400

    Added careers contact

--- portfolio/components/career.js
+++ portfolio/components/career.js
@@ -5,7 +5,16 @@ function Careers () {
   return (
     <div className="Careers">
       <header className="Careers-header">
+      <h1>Greg's Career</h1>
+
+       <h1>Validas</h1>
+        <p>Data Analyst</p>
        
+       <h1>AT&T</h1>
+       <p>Corporate Sales</p>
+       <h1>Verizon Wireless</h1>
+       <p>Corproate Sales</p>
+
       </header>
     </div>
   );
