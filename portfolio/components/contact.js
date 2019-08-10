59cbb1ba4ac1053277e45d5f33e24b9efd9a295f
GregoryUrban <turknben@gmail.com>
2019-08-09 21:33:39 -0400

    Added pic and design directory, adjusted contact structure

--- portfolio/components/contact.js
+++ portfolio/components/contact.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import Greg from '../../design-files/greg.jpg'
 
 function Contact () {
   return (
     <div className="Contact">
       <header className="Contact-header">
-       
+        <h1>How to reach Greg Urban</h1>
+      <email>
+        GregTheUrban@gmail.com
+      </email>
+      <phonenumber>
+           802-246-7026
+         </phonenumber>
+      <address>
+           Brattleboro, VT 05301
+         </address>
+
+         <img src = 'Greg'></img>
+   
       </header>
     </div>
   );
